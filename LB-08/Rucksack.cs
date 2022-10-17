6fd304f99854b6bf057daf9e061d6acc4b7a6cd1
Skiif64 <86207504+Skiif64@users.noreply.github.com>
2022-10-18 00:10:28 +0400

    Убраны коменты

--- LB-08/Rucksack.cs
+++ LB-08/Rucksack.cs
@@ -7,13 +7,12 @@
             int k = items.Length;
             int[,] a = new int[k + 1, capacity + 1];
             for (int s = 1; s <= k; s++)
-            {
-                // s - максимальный номер предмета, который можно использовать
+            {                
                 for (int n = 1; n <= capacity; n++)
-                {// n - вместимости рюкзака
+                {
                     a[s, n] = a[s - 1, n];
-                    if (n >= items[s - 1].Weight && (a[s - 1, n - items[s - 1].Weight] + items[s - 1].Value >
-                    a[s, n])) a[s, n] = a[s - 1, n - items[s - 1].Weight] + items[s - 1].Value;
+                    if (n >= items[s - 1].Weight && (a[s - 1, n - items[s - 1].Weight] + items[s - 1].Value > a[s, n]))
+                        a[s, n] = a[s - 1, n - items[s - 1].Weight] + items[s - 1].Value;
                 }
             }
 
